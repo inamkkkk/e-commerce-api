75624f5bbee3704ef978b6376a40580d43cbcf1e
inamkkkk <inamk9025@gmail.com>
2025-09-22 00:49:24 +0500

    fix: refine src/middlewares/validationMiddleware.js

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -3,6 +3,7 @@ const createHttpError = require('http-errors');
 const validate = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body, { abortEarly: false });
   if (error) {
+    // TODO: Enhance error messages to be more specific, possibly including field names.
     const message = error.details.map((detail) => detail.message).join(', ');
     return next(createHttpError(400, message));
   }
