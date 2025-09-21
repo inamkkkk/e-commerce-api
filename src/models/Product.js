9a573a96dbd6225409e9f47899f2675c7c457393
inamkkkk <inamk9025@gmail.com>
2025-09-22 00:44:03 +0500

    fix: refine src/models/Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -9,22 +9,17 @@ const ProductSchema = new mongoose.Schema({
   },
   description: {
     type: String,
-    required: [true, 'Product description is required'],
-  },
+    required: [true, 'Product description is required']},
   price: {
     type: Number,
     required: [true, 'Product price is required'],
-    min: [0, 'Price cannot be negative'],
-  },
+    min: [0, 'Price cannot be negative']},
   stock: {
     type: Number,
     required: [true, 'Product stock is required'],
-    min: [0, 'Stock cannot be negative'],
-  },
+    min: [0, 'Stock cannot be negative']},
   createdAt: {
     type: Date,
-    default: Date.now,
-  },
-});
+    default: Date.now}});
 
 module.exports = mongoose.model('Product', ProductSchema);
