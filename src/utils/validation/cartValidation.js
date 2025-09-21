b22af38fab92e337eff2e5533044c573aaacd75d
inamkkkk <inamk9025@gmail.com>
2025-09-22 00:51:01 +0500

    fix: refine src/utils/validation/cartValidation.js

--- src/utils/validation/cartValidation.js
+++ src/utils/validation/cartValidation.js
@@ -8,18 +8,21 @@ const itemSchema = Joi.object({
     }
     return value;
   }, 'ObjectId validation').required(),
-  quantity: Joi.number().integer().min(1).required(),
-});
+  quantity: Joi.number().integer().min(1).required()});
 
 const addToCartSchema = Joi.object({
-  items: Joi.array().items(itemSchema).min(1).required(),
-});
+  items: Joi.array().items(itemSchema).min(1).required()});
 
 const removeFromCartSchema = Joi.object({
-  items: Joi.array().items(itemSchema).min(1).required(), // Can remove multiple items or specify quantity to remove
-});
+  // We can assume that if an item is in the cart, its productId is valid.
+  // However, the quantity to remove should also be validated.
+  items: Joi.array().items(
+    Joi.object({
+      productId: Joi.string().required(), // Assuming productId already exists in the cart
+      quantity: Joi.number().integer().min(1).required(), // Quantity to remove
+    })
+  ).min(1).required()});
 
 module.exports = {
   addToCartSchema,
-  removeFromCartSchema,
-};
+  removeFromCartSchema};
